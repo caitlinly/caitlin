d3dcca8b8571c3caebd00d439fa977bf2b841555
caitlinly <43999437+caitlinly@users.noreply.github.com>
2018-12-21 11:32:01 -0800

    caitlin

---
+++ Friend Book/src/sample/Controller.java
@@ -0,0 +1,10 @@
+//create a friend
+//Select a friend and see their properties
+//Delete a friend
+//Display all the friends
+//Friends should be objects 
+
+package sample;
+
+public class Controller {
+}
